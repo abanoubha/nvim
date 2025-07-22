18556f72f20d3a2e0de3790bacff5cf4fc4951a3
Abanoub Hanna <abanoubhannaa73@gmail.com>
2025-07-22 11:07:48 +0300

    feature: press ENTER to select from textual menu of autocomplete

--- init.lua
+++ init.lua
@@ -669,6 +669,7 @@ require("lazy").setup({
 					--  This will auto-import if your LSP supports it.
 					--  This will expand snippets if the LSP sent a snippet.
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- ENTER (or RETURN)
 
 					-- If you prefer more traditional completion keymaps,
 					-- you can uncomment the following lines
