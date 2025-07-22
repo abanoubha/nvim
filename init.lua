bf40c8af9b4bcf86d59c417b28927399c57e7849
Abanoub Hanna <abanoubhannaa73@gmail.com>
2025-07-22 12:11:38 +0300

    fix: do not break words when wrapping

--- init.lua
+++ init.lua
@@ -31,6 +31,9 @@ vim.opt.clipboard = "unnamedplus"
 -- Enable break indent
 vim.opt.breakindent = true
 
+-- Enable line break
+vim.opt.linebreak = true
+
 -- Save undo history
 vim.opt.undofile = true
 
